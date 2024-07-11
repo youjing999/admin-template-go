f8497ac5c91a538f8f42558210c606f1d7c54c47
youjing <youjing999@qq.com>
2024-07-11 14:09:53 +0800

    feat: config file

--- common/config/config.go
+++ common/config/config.go
@@ -7,13 +7,46 @@ import (
 
 //总配置文件
 type config struct {
-	server server `yaml:"server"`
+	server        server        `yaml:"server"`
+	Db            db            `yaml:"db"`
+	redis         redis         `yaml:"redis"`
+	log           log           `yaml:"log"`
+	imageSettings imageSettings `yaml:"imageSettings"`
 }
 
 // 项目端口配置
 type server struct {
 	Address string `yaml:"address"`
 	Model   string `yaml:"model"`
+}
+
+type db struct {
+	Dialects string `yaml:"dialects"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Db       string `yaml:"db"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Charset  string `yaml:"charset"`
+	MaxIdle  int    `yaml:"maxIdle"`
+	MaxOpen  int    `yaml:"maxOpen"`
+}
+
+type redis struct {
+	Address string `yaml:"address"`
+	//Password string `yaml:"password"`
+}
+
+type log struct {
+	Path  string `yaml:"path"`
+	Name  string `yaml:"name"`
+	Model string `yaml:"model"`
+}
+
+// image图片上传设置
+type imageSettings struct {
+	UploadDir string `yaml:"uploadDir"`
+	ImageHost string `yaml:"imageHost"`
 }
 
 var Config *config
