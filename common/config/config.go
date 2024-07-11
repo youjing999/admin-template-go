14847166fd11df60e21550bcca345bed854ab738
youjing <youjing999@qq.com>
2024-07-11 13:45:53 +0800

    feat: port and init

--- common/config/config.go
+++ common/config/config.go
@@ -1,7 +1,31 @@
 package config
 
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+//总配置文件
+type config struct {
+	server server `yaml:"server"`
+}
+
 // 项目端口配置
 type server struct {
 	Address string `yaml:"address"`
 	Model   string `yaml:"model"`
+}
+
+var Config *config
+
+func init() {
+	yamlFile, err := ioutil.ReadFile("./config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &Config)
+	if err != nil {
+		panic(err)
+	}
 }
